611bb47bd12ee6bbbd8d583fcb4c9fca3c8423fa
manis-h <85171340+manis-h@users.noreply.github.com>
2026-01-29 18:04:59 +0530

    Replace Math Magicians project with Proctoring App

--- src/constants/index.js
+++ src/constants/index.js
@@ -205,27 +205,27 @@ const projects = [
     // repo: 'https://github.com/shaqdeff/Leaderboard',
     demo: 'https://retro-games4u.netlify.app/',
   },
-  // {
-  //   id: 'project-3',
-  //   name: 'Math Magicians',
-  //   description: 'This is a single-page calculator app built with React',
-  //   tags: [
-  //     {
-  //       name: 'nextjs',
-  //       color: 'blue-text-gradient',
-  //     },
-  //     {
-  //       name: 'supabase',
-  //       color: 'green-text-gradient',
-  //     },
-  //     {
-  //       name: 'css',
-  //       color: 'pink-text-gradient',
-  //     },
-  //   ],
-  //   image: math,
-  //   repo: 'https://github.com/shaqdeff/Math-Magicians',
-  //   demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+   {
+     id: 'project-3',
+     name: 'Proctoring App',
+     description: 'This is a single-page calculator app built with React',
+     tags: [
+       {
+         name: 'nextjs',
+         color: 'blue-text-gradient',
+       },
+       {
+        name: 'supabase',
+         color: 'green-text-gradient',
+       },
+       {
+         name: 'css',
+         color: 'pink-text-gradient',
+       },
+     ],
+     image: math,
+    // repo: 'https://github.com/shaqdeff/Math-Magicians',
+    demo: 'https://procture.netlify.app/',
   // },
   {
     id: 'project-4',
